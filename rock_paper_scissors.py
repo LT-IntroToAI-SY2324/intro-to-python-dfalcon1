d60eed227744e0e4222c5282b0d004c64cdff316
Daniel Falcon <dfalcon1.edu>
2023-09-07 10:40:25 -0500

    Added Deermine Victor

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -6,18 +6,29 @@ computer_choice = "paper"
 
 #make a function that gets the choices
 def get_choices():
-    options = ['fire', 'water', 'grass']
+    options = ["fire","water","grass"]
 
-    Player_choice = input("choose between fire, water, or grass. ")
+    Player_choice = input("choose between fire, water, or grass.")
     computer_choice = random.choice(options)
     choices = {"player": Player_choice, "computer":computer_choice}
-
     return choices
 
 choices = get_choices()
 print()
 print(choices)
 print()
-if "Player" == "fire":
-    if "computer" == 'water':
-        print("you lose")
+def type_advantages():
+    if choices == {"player": "fire", "computer":"water"}:
+        print("You Lose")
+    elif choices == {"player": "fire", "computer":"grass"}:
+        print("You win")
+    elif choices == {"player": "fire", "computer":"fire"}:
+        
+        print("You tied")
+     if choices == {"player": "water", "computer":"water"}:
+        print("You tied")
+    elif choices == {"player": "water", "computer":"grass"}:
+        print("You Lose")
+    elif choices == {"player": "water", "computer":"fire"}:
+        print("You win")
+        
